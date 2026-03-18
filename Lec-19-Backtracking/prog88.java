a5bf8f021cb643d77336d9883ce32a9b130fa628
Aditya Ingle <adityaingle1420@gmail.com>
2026-03-18 23:51:43 +0530

    add Backtracking

---
+++ Lec-19-Backtracking/prog88.java
@@ -0,0 +1,27 @@
+// Backtracking 
+
+public class prog88 {
+    
+    public static void printPermutation(String str , String perm , int index){
+
+        //base case
+        if(str.length() == 0){
+            System.out.println(perm);
+            return;
+        }
+
+        for(int i = 0 ; i<str.length() ; i++){
+            char currChar = str.charAt(i);
+            String newStr = str.substring(0,i) + str.substring(i+1);
+            printPermutation(str,perm + currChar , index +1);
+        }
+    }
+
+    public static void main(String args[]){
+        String str = "ABC";
+        printPermutation(str,"",0);
+    }
+
+
+}
+ 
